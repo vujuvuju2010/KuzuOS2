008c2abc1f7b6a53f67d284d8ed96dbf1936747a
Vujuvuju <102369725+vujuvuju2010@users.noreply.github.com>
2025-12-27 21:50:18 +0300

    CROWBARRRRR

--- src/fatfs/integer.h
+++ src/fatfs/integer.h
@@ -1,4 +1,1 @@
-/*-------------------------------------------*/
-/* Integer type definitions for FatFs module */
-/*-------------------------------------------*/
-// ... (FatFs orijinal integer.h tam içeriği buraya eklenecek) ... 
+// basically placeholder DO NOT DELETE look these guys are here for a reason they will caryy your painnnn (crowbar refferance btw) LONG STORY SHORT THEE SHALL NOT DELETE ANYTHING IN THIS FOLDER 
